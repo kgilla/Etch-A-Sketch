9f0ff9e9641ddc1c8921d2a07ff276f69d855c46
kenneth <gilmorekenneth@gmail.com>
2020-02-16 15:17:23 -0500

    Added button functions

--- code.js
+++ code.js
@@ -1,24 +1,65 @@
+// default build
 const main = document.querySelector('#mainBox');
-const button = document.querySelector('button');
+let divNum = 16;
+builder();
+eventSetter();
 
+// defines and adds erase button
+const buttonErase = document.querySelector('#buttonErase');
+buttonErase.addEventListener('click', eraser);
 
-//number selected by user to populate grid
-const divNum = 64;
-const divTotal = divNum * divNum;
+// assigns events for all size buttons
+const buttonSize = document.querySelectorAll('.btn-secondary');
+buttonSize.forEach((buttonSize) => {
+    buttonSize.addEventListener('click', build, event);
+});
 
-main.style.gridTemplateColumns = `repeat(${divNum}, 1fr)`;
-main.style.gridTemplateRows = `repeat(${divNum}, 1fr)`;
-
-for (let i = 0; i < divTotal; i++) {
-    const box = document.createElement('div');
-    box.classList.add('etchBox');
-    main.appendChild(box);
+// resizes grid based on user input
+function build() {
+    eraser();
+    if (event.target.id === 'button128') {
+        divNum = 128;
+    } else if (event.target.id === 'button64') {
+        divNum = 64;
+    } else if (event.target.id === 'button32') {
+        divNum = 32;
+    } else if (event.target.id === 'button16') {
+        divNum = 16;
+    }
+    builder();
+    eventSetter();
 }
 
-const etchBox = document.querySelectorAll('.etchBox');
-etchBox.forEach((etchBox) => { 
-    etchBox.addEventListener('mouseover', (e) => {
-        etchBox.style.backgroundColor = 'black';
+// builds the grid
+function builder() {
+    let divTotal = divNum * divNum;
+    main.style.gridTemplateColumns = `repeat(${divNum}, 1fr)`;
+    main.style.gridTemplateRows = `repeat(${divNum}, 1fr)`;
+    for (let i = 0; i < divTotal; i++) {
+        const box = document.createElement('div');
+        box.classList.add('etchBox');
+        main.appendChild(box);
+    }
+}
+
+// sets events for all elements in grid
+function eventSetter() {
+    const etchBox = document.querySelectorAll('.etchBox');
+    etchBox.forEach((etchBox) => { 
+        etchBox.addEventListener('mouseover', (e) => {
+            etchBox.style.backgroundColor = 'black';
+        });
     });
-});
+}
+
+// resets the grid 
+function eraser() {
+    let etchBox = document.querySelectorAll('.etchBox');
+    etchBox.forEach((etchBox) => {
+        if (etchBox.style.backgroundColor === 'black') {
+            etchBox.style.backgroundColor = 'gray';
+        }
+    });
+}
+
 
